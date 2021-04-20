bf33d3dd8bb625d815d31ba0f754580674c16c46
Richard <richard760x@hotmail.com>
2021-04-19 23:38:43 -0400

    moved dmenu's color scheme definition of; white, black, orange from dwm to dmenu

--- dwm/config.h
+++ dwm/config.h
@@ -62,8 +62,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] =
-  { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_white, "-sb", col_orange, "-sf", col_black, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "mate-terminal", NULL };
 
 #include "movestack.c"
